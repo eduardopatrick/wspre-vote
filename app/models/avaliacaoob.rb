3f62fddf4e1eb147c9e95657f94c412d5c5331bc
Matheus S. Araújo <matuf2008@hotmail.com>
2018-03-25 14:15:12 -0300

    Update avaliacaoob.rb

--- app/models/avaliacaoob.rb
+++ app/models/avaliacaoob.rb
@@ -2,7 +2,7 @@ class Avaliacaoob < ApplicationRecord
   attr_accessor :idobra
   attr_accessor :cpf
 
-   validates :cpf, uniqueness: { scope: :nota,
+   validates :cpf, uniqueness: { scope: :idobra,
        message: "Só acontece de cpf uma vez por idobra" } #Return true with SUCESS and return false with ERROR
 
   Paperclip.interpolates :idobra do |attachment, style|
